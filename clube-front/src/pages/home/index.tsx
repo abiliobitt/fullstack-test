6755ba41d008285f29c5df5830e2fac9a5c62f4f
Abilio Bittencourt <abilio.jr@live.com>
2023-11-21 13:03:21 -0300

    feat: create a home page to create a base interface with generic data

---
+++ clube-front/src/pages/home/index.tsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import {
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar
+} from '@mui/material/';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+
+import Header from '../../components/ui/header';
+import Modal from '../../components/ui/modal';
+import ClientList from '../../components/common/clientList';
+import Logo from '../../assets/img/logo.jpg'
+
+const drawerWidth = 240;
+
+interface Props {
+}
+
+const Home = (props: Props) => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [modal, setModal] = useState(false);
+  const Toggle = () => setModal(!modal);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <img src={Logo} alt="Logotipo" />
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton onClick={Toggle}>
+            <ListItemIcon>
+              <PersonAddIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Cadastrar Usuário"} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  return (
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Header handleDrawerToggle={handleDrawerToggle} />
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true,
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+          >
+            {drawer}
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        >
+          <Toolbar />
+          <ClientList />
+        </Box>
+      </Box>
+      <Modal show={modal} close={Toggle} title="Dynamic Title">
+        This is modal content
+      </Modal>
+    </>
+  );
+}
+
+export default Home
